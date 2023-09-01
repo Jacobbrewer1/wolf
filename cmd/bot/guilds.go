7cee86bab63818fd64fb312c06c83a5d9679f794
Jacob Brewer <Jacob.brewer@bthree.uk>
2023-09-01 11:37:06 +0100

    Correct the registering of slash commands

--- cmd/bot/guilds.go
+++ cmd/bot/guilds.go
@@ -5,20 +5,30 @@ import (
 
 	"github.com/Jacobbrewer1/discordgo"
 	"github.com/Jacobbrewer1/wolf/cmd/bot/monitoring"
+	"github.com/Jacobbrewer1/wolf/pkg/logging"
+	"golang.org/x/exp/slog"
 )
 
-func guildJoinedHandler(a IApp) func(s *discordgo.Session, g *discordgo.GuildCreate) {
+func (a *App) guildJoinedHandler() func(s *discordgo.Session, g *discordgo.GuildCreate) {
 	return func(_ *discordgo.Session, g *discordgo.GuildCreate) {
 		a.Log().Info(fmt.Sprintf("Joined guild %s", g.Name))
+
+		if err := a.registerSlashCommands(); err != nil {
+			a.Log().Error("Error registering slash commands", slog.String(logging.KeyError, err.Error()))
+		}
 
 		// Increment the total number of guilds.
 		monitoring.TotalDiscordGuilds.Inc()
 	}
 }
 
-func guildLeaveHandler(a IApp) func(s *discordgo.Session, g *discordgo.GuildDelete) {
+func (a *App) guildLeaveHandler() func(s *discordgo.Session, g *discordgo.GuildDelete) {
 	return func(_ *discordgo.Session, g *discordgo.GuildDelete) {
 		a.Log().Info(fmt.Sprintf("Left guild %s", g.Name))
+
+		if err := a.unregisterSlashCommands(); err != nil {
+			a.Log().Error("Error unregistering slash commands", slog.String(logging.KeyError, err.Error()))
+		}
 
 		// Decrement the total number of guilds.
 		monitoring.TotalDiscordGuilds.Dec()
